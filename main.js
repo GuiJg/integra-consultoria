4219680e6cfb00d744dc27cea57df3fb3dbb5377
GuiJg <j.gu1lh3rm325@gmail.com>
2023-06-11 21:10:44 -0300

    fourth

--- main.js
+++ main.js
@@ -20,10 +20,20 @@ window.addEventListener('scroll', function(){
 const button = document.querySelector('.btn-home');
 let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 
-if (scrollPosition > 100) {
-  button.style.opacity = '1';
-} else {
-  button.style.opacity = '0';
-}
+  if (scrollPosition > 100) {
+    button.style.opacity = '1';
+  }
+  else {
+    button.style.opacity = '0';
+  }
+  
 });
+
+const menuButton = document.querySelector('.menu-button');
+const headerMenu = document.querySelector('.header-menu');
+
+  menuButton.addEventListener('click', function() {
+    headerMenu.classList.toggle('active');
+  });
+
 
