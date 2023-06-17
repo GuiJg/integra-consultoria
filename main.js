7102ab82183833baafa2d99a400ad42f7e19ec2d
GuiJg <j.gu1lh3rm325@gmail.com>
2023-06-17 09:50:51 -0300

    fifth

--- main.js
+++ main.js
@@ -36,4 +36,57 @@ const headerMenu = document.querySelector('.header-menu');
     headerMenu.classList.toggle('active');
   });
 
+  let item = document.getElementById("item")
+  let contracts = document.getElementById("contracts")
+  let company = document.getElementById("company")
+  let text = document.getElementById("text")
+  let title = document.getElementById("title")
+  
+    window.addEventListener("scroll", function(){
+      const sectionTop = item.getBoundingClientRect().top;
+      const sectionMid = contracts.getBoundingClientRect().top;
+      const sectionSa = company.getBoundingClientRect().top;
+      const sectionText = text.getBoundingClientRect().top;
+      const sectionTitle = title.getBoundingClientRect().top;
+      const windowHeight=window.innerHeight;
+  
+      if(sectionTop < windowHeight){
+        item.classList.add("ativa");
+      }
+  
+      else{
+        item.classList.remove("ativa");
+      }
+      
+      if(sectionMid < windowHeight){
+        contracts.classList.add("ativa");
+      }
+  
+      else{
+        contracts.classList.remove("ativa");
+      }
 
+      if(sectionSa < windowHeight){
+        company.classList.add("ativa");
+      }
+  
+      else{
+        company.classList.remove("ativa");
+      }
+
+      if(sectionText < windowHeight){
+        text.classList.add("ativa");
+      }
+  
+      else{
+        text.classList.remove("ativa");
+      }
+  
+      if(sectionTitle < windowHeight){
+        title.classList.add("ativa");
+      }
+  
+      else{
+        title.classList.remove("ativa");
+      }
+    });
